e8ce157751c20fd74009ba4d73d0d77f5532bd0f
David Cascos <davidcascosgimeno@gmail.com>
2020-05-20 21:48:33 +0200

    Created Rules class

---
+++ app/src/main/java/com/dcascos/connect4/layouts/Rules.java
@@ -0,0 +1,30 @@
+package com.dcascos.connect4.layouts;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.dcascos.connect4.R;
+
+public class Rules extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.ac_rules);
+
+		ImageButton bt_back = findViewById(R.id.bt_back);
+		final MediaPlayer click = MediaPlayer.create(this, R.raw.click);
+
+		bt_back.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				click.start();
+				finish();
+			}
+		});
+	}
+}
