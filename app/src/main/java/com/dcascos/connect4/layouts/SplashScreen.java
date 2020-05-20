41e37529c4ed8bed75eac4256e52440be3033a68
David Cascos <davidcascosgimeno@gmail.com>
2020-05-20 21:16:22 +0200

    Added intent in splashScreen to access MainMenu

--- app/src/main/java/com/dcascos/connect4/layouts/SplashScreen.java
+++ app/src/main/java/com/dcascos/connect4/layouts/SplashScreen.java
@@ -24,8 +24,8 @@ public class SplashScreen extends AppCompatActivity {
 		new Handler().postDelayed(new Runnable() {
 			@Override
 			public void run() {
-				//Intent intent = new Intent(SplashScreen.this, MainMenu.class);
-				//startActivity(intent);
+				Intent intent = new Intent(SplashScreen.this, MainMenu.class);
+				startActivity(intent);
 				finish();
 			}
 		}, time);
