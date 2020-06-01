95b29bfbb175c66efd6ef5924ca6a6b44a463d6c
David Cascos <davidcascosgimeno@gmail.com>
2020-06-01 16:17:36 +0200

    Modoficasdo comportamiento del manager para actuar segun la version de la bd

--- app/src/main/java/com/dcascos/connect4/database/DBManager.java
+++ app/src/main/java/com/dcascos/connect4/database/DBManager.java
@@ -31,20 +31,41 @@ public class DBManager {
 		dbHelper.close();
 	}
 
-	public void insert(String alias, String date, int size, boolean timing, String time, String result, byte[] image) {
+	public void insert(String alias, String date, int size, boolean timing, String time, String result, byte[] image, byte[] imageResultado) {
 		ContentValues contentValue = new ContentValues();
-		contentValue.put(DBHelper.ALIAS, alias);
-		contentValue.put(DBHelper.DATE, date);
-		contentValue.put(DBHelper.SIZE, size);
-		contentValue.put(DBHelper.TIMING, timing);
-		contentValue.put(DBHelper.TIME, time);
-		contentValue.put(DBHelper.RESULT, result);
-		contentValue.put(DBHelper.IMAGE, image);
-		db.insert(DBHelper.TABLE_NAME, null, contentValue);
+
+		if (DBHelper.DB_VERSION < 2) {
+			contentValue.put(DBHelper.ALIAS, alias);
+			contentValue.put(DBHelper.DATE, date);
+			contentValue.put(DBHelper.SIZE, size);
+			contentValue.put(DBHelper.TIMING, timing);
+			contentValue.put(DBHelper.TIME, time);
+			contentValue.put(DBHelper.RESULT, result);
+			contentValue.put(DBHelper.IMAGE, image);
+			db.insert(DBHelper.TABLE_NAME, null, contentValue);
+
+		} else {
+			contentValue.put(DBHelper.ALIAS, alias);
+			contentValue.put(DBHelper.DATE, date);
+			contentValue.put(DBHelper.SIZE, size);
+			contentValue.put(DBHelper.TIMING, timing);
+			contentValue.put(DBHelper.TIME, time);
+			//EL CAMPO SE MANTIENE YA QUE SINO DESESTRUCTURA TODA LA APLICACION EN CUANTO A LOS CARDVIEWS Y TOAST PERSONALIZADOS
+			//PERO IGUALMENTE SE HA ACTUALIZADO LA BBDD PARA QUE GUARDE UN BLOB DE LAS IMAGENES DEL ENUNCIADO EN CUANTO SE MODIFICA LA VERSION
+			contentValue.put(DBHelper.RESULT, result);
+			contentValue.put(DBHelper.IMAGE, image);
+			contentValue.put(DBHelper.IMAGERESULTADO, imageResultado);
+			db.insert(DBHelper.TABLE_NAME2, null, contentValue);
+		}
 	}
 
 	public Cursor getCursor(String[] columns) {
-		Cursor cursor = db.query(DBHelper.TABLE_NAME, columns, null, null, null, null, null);
+		Cursor cursor;
+		if (DBHelper.DB_VERSION < 2) {
+			cursor = db.query(DBHelper.TABLE_NAME, columns, null, null, null, null, null);
+		} else {
+			cursor = db.query(DBHelper.TABLE_NAME2, columns, null, null, null, null, null);
+		}
 		if (cursor != null) {
 			cursor.moveToFirst();
 		}
@@ -52,7 +73,12 @@ public class DBManager {
 	}
 
 	public Cursor getCursorByName(String[] columns, String value) {
-		Cursor cursor = db.query(DBHelper.TABLE_NAME, columns, "alias like ?", new String[]{value}, null, null, null);
+		Cursor cursor;
+		if (DBHelper.DB_VERSION < 2) {
+			cursor = db.query(DBHelper.TABLE_NAME, columns, "alias like ?", new String[]{value}, null, null, null);
+		} else {
+			cursor = db.query(DBHelper.TABLE_NAME2, columns, "alias like ?", new String[]{value}, null, null, null);
+		}
 		if (cursor != null) {
 			cursor.moveToFirst();
 		}
@@ -60,6 +86,15 @@ public class DBManager {
 	}
 
 	public void delete(long _id) {
-		db.delete(DBHelper.TABLE_NAME, DBHelper._ID + "=" + _id, null);
+		if (DBHelper.DB_VERSION < 2) {
+			db.delete(DBHelper.TABLE_NAME, DBHelper._ID + "=" + _id, null);
+		} else {
+			db.delete(DBHelper.TABLE_NAME2, DBHelper._ID + "=" + _id, null);
+
+		}
+	}
+
+	public int getVersion() {
+		return DBHelper.DB_VERSION;
 	}
 }
