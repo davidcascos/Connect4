ddd0cf46bd951b57632f7905ed459305da095887
David Cascos <davidcascosgimeno@gmail.com>
2020-06-01 16:15:54 +0200

    Adaptado fragment de la version 1 para recuperar bitmap

--- app/src/main/java/com/dcascos/connect4/fragments/ResultPFrag.java
+++ app/src/main/java/com/dcascos/connect4/fragments/ResultPFrag.java
@@ -1,6 +1,7 @@
 package com.dcascos.connect4.fragments;
 
 import android.database.Cursor;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,7 +20,8 @@ import com.dcascos.connect4.logic.Status;
 public class ResultPFrag extends Fragment {
 	private ImageView iv_resultado;
 	String status;
-
+	byte[] image;
+	DBManager dbManager;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -36,44 +38,76 @@ public class ResultPFrag extends Fragment {
 		super.onActivityCreated(savedInstanceState);
 
 		if (savedInstanceState != null) {
-			status = savedInstanceState.getString(getString(R.string.keyResult));
+			dbManager = new DBManager(getActivity());
+			dbManager.openRead();
+			if (dbManager.getVersion() < 2) {
+				status = savedInstanceState.getString(getString(R.string.keyResult));
+			} else {
+				image = savedInstanceState.getByteArray(getString(R.string.keyImage));
+			}
 			fillCamps();
 		}
 	}
 
 	public void showImage(int position) {
-		DBManager dbManager = new DBManager(getActivity());
+		dbManager = new DBManager(getActivity());
 		dbManager.openRead();
-		String[] columns = new String[]{DBHelper._ID, DBHelper.RESULT};
-		Cursor cursor = dbManager.getCursor(columns);
+		String[] columns;
+		Cursor cursor;
 
-		if (cursor.moveToFirst()) {
-			cursor.moveToPosition(position);
+		if (dbManager.getVersion() < 2) {
+			columns = new String[]{DBHelper._ID, DBHelper.RESULT};
+			cursor = dbManager.getCursor(columns);
+
+			if (cursor.moveToFirst()) {
+				cursor.moveToPosition(position);
+			}
+			status = cursor.getString(1);
+
+		} else {
+			columns = new String[]{DBHelper._ID, DBHelper.IMAGERESULTADO};
+			cursor = dbManager.getCursor(columns);
+
+			if (cursor.moveToFirst()) {
+				cursor.moveToPosition(position);
+			}
+
+			image = cursor.getBlob(1);
 		}
-
-		status = cursor.getString(1);
 		fillCamps();
-		dbManager.close();
 	}
 
 	private void fillCamps() {
 		iv_resultado = getActivity().findViewById(R.id.iv_resultado);
 
-		if (status.equals(Status.PLAYER1_WINS.toString())) {
-			iv_resultado.setImageResource(R.drawable.victoria);
-		} else if (status.equals(Status.PLAYER2_WINS.toString())) {
-			iv_resultado.setImageResource(R.drawable.derrota);
-		} else if (status.equals(Status.DRAW.toString())) {
-			iv_resultado.setImageResource(R.drawable.empate);
-		} else if (status.equals(Status.ALL_PLAYER_LOSE.toString())) {
-			iv_resultado.setImageResource(R.drawable.tiempoagotado);
+		if (dbManager.getVersion() < 2) {
+			if (status.equals(Status.PLAYER1_WINS.toString())) {
+				iv_resultado.setImageResource(R.drawable.victoria);
+			} else if (status.equals(Status.PLAYER2_WINS.toString())) {
+				iv_resultado.setImageResource(R.drawable.derrota);
+			} else if (status.equals(Status.DRAW.toString())) {
+				iv_resultado.setImageResource(R.drawable.empate);
+			} else if (status.equals(Status.ALL_PLAYER_LOSE.toString())) {
+				iv_resultado.setImageResource(R.drawable.tiempoagotado);
+			}
+		} else {
+			iv_resultado.setImageBitmap(BitmapFactory.decodeByteArray(image, 0, image.length));
 		}
 	}
 
 	@Override
 	public void onSaveInstanceState(@NonNull Bundle outState) {
 		super.onSaveInstanceState(outState);
+		if (dbManager.getVersion() < 2) {
+			outState.putString(getString(R.string.keyResult), status);
+		} else {
+			outState.putByteArray(getString(R.string.keyImage), image);
+		}
+	}
 
-		outState.putString(getString(R.string.keyResult), status);
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		dbManager.close();
 	}
 }
